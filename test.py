58578cfb36ded1e3fca9ae1d3e4170a5f32bb1d8
server <server@server.com>
2017-01-28 11:59:42 +0530

    added test.py that has addition function which will be used in main.py

---
+++ test.py
@@ -0,0 +1,6 @@
+import logging  # import logging
+
+def add(a,b):
+    '''Simple addition fiction that takes two parameters and add'''
+    logging.info(a + b)  # logger built-in function that confirms
+    # the function is working as it should be.
